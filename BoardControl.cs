3c3a995d85aa8c4fc40556f1a9d7cead2675f893
Kristoffer Danbrant <krda15@student.bth.se>
2023-03-27 21:52:11 +0200

    updated BoardControl

--- BoardControl.cs
+++ BoardControl.cs
@@ -1,232 +1,131 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Minesweeper;
+using System.Resources;
 using System.Windows.Forms;
+using System;
 
-namespace Minesweeper
+public class BoardControl : UserControl
 {
-    public class BoardControl : Panel
+    private GameController gameController;
+    private CellControl[,] cellControls;
+
+    public BoardControl(int rows, int cols, int numMines)
     {
-        private Board board;
-        private CellControl[,] cellControls;
+        CreateGameBoard(rows, cols, numMines);
+        CreateCellControls(rows, cols);
+    }
 
-        public event EventHandler GameOver;
+    private void CreateGameBoard(int rows, int cols, int numMines)
+    {
+        gameController = new GameController(rows, cols, numMines);
+    }
 
-        public BoardControl(Board board)
+    private void CreateCellControls(int rows, int cols)
+    {
+        cellControls = new CellControl[rows, cols];
+        for (int row = 0; row < rows; row++)
         {
-            this.board = board;
-            cellControls = new CellControl[board.Rows, board.Columns];
-            Dock = DockStyle.Fill;
+            for (int col = 0; col < cols; col++)
+            {
+                var cellControl = new CellControl(row, col);
+                cellControl.LeftClick += OnCellLeftClick;
+                cellControl.RightClick += OnCellRightClick;
+                Controls.Add(cellControl);
+                cellControls[row, col] = cellControl;
+            }
+        }
+    }
 
-            InitializeCells();
-            SetCellNeighbors();
+    private void OnCellLeftClick(object sender, EventArgs e)
+    {
+        var cellControl = (CellControl)sender;
+        gameController.RevealCell(cellControl.Row, cellControl.Col);
+        UpdateBoard();
+    }
+
+    private void OnCellRightClick(object sender, EventArgs e)
+    {
+        var cellControl = (CellControl)sender;
+        gameController.ToggleFlag(cellControl.Row, cellControl.Col);
+        UpdateBoard();
+    }
+
+    private void UpdateBoard()
+    {
+        for (int row = 0; row < gameController.Rows; row++)
+        {
+            for (int col = 0; col < gameController.Columns; col++)
+            {
+                UpdateCellState(row, col);
+            }
         }
 
-        private void InitializeCells()
+        // Update timer, mine counter, etc.
+    }
+
+    private void UpdateCellState(int row, int col, CellState cellState)
+    {
+        // Get the cell control for the specified row and column
+        CellControl cellControl = cellControls[row, col];
+
+        // Set the cell state based on the game state
+        switch (cellState)
         {
-            for (int row = 0; row < board.Rows; row++)
-            {
-                for (int col = 0; col < board.Columns; col++)
+            case CellState.Hidden:
+                cellControl.BackgroundImage = Resources.hidden;
+                cellControl.Enabled = true;
+                break;
+            case CellState.Flagged:
+                cellControl.BackgroundImage = Resources.flag;
+                cellControl.Enabled = true;
+                break;
+            case CellState.QuestionMark:
+                cellControl.BackgroundImage = Resources.question;
+                cellControl.Enabled = true;
+                break;
+            case CellState.Revealed:
+                int adjacentMines = gameController.GetAdjacentMines(row, col);
+                cellControl.Enabled = false;
+                cellControl.SetAdjacentMines(adjacentMines);
+
+                switch (adjacentMines)
                 {
-                    var cell = board.Cells[row, col];
-                    var cellControl = new CellControl
-                    {
-                        IsMine = cell.IsMine,
-                        AdjacentMines = cell.AdjacentMines
-                    };
-                    cellControls[row, col] = cellControl;
-                    cellControl.MouseDown += CellControl_MouseDown;
-                    Controls.Add(cellControl, col, row);
+                    case 0:
+                        cellControl.BackgroundImage = Resources.zero;
+                        break;
+                    case 1:
+                        cellControl.BackgroundImage = Resources.one;
+                        break;
+                    case 2:
+                        cellControl.BackgroundImage = Resources.two;
+                        break;
+                    case 3:
+                        cellControl.BackgroundImage = Resources.three;
+                        break;
+                    case 4:
+                        cellControl.BackgroundImage = Resources.four;
+                        break;
+                    case 5:
+                        cellControl.BackgroundImage = Resources.five;
+                        break;
+                    case 6:
+                        cellControl.BackgroundImage = Resources.six;
+                        break;
+                    case 7:
+                        cellControl.BackgroundImage = Resources.seven;
+                        break;
+                    case 8:
+                        cellControl.BackgroundImage = Resources.eight;
+                        break;
+                    default:
+                        throw new InvalidOperationException("Invalid number of adjacent mines.");
                 }
-            }
-        }
-
-        private void SetCellNeighbors()
-        {
-            for (int row = 0; row < board.Rows; row++)
-            {
-                for (int col = 0; col < board.Columns; col++)
-                {
-                    var cell = board.Cells[row, col];
-                    var cellControl = cellControls[row, col];
-
-                    // Set the neighbor cells for this cell
-                    var neighbors = new List<Cell>();
-                    for (int i = -1; i <= 1; i++)
-                    {
-                        for (int j = -1; j <= 1; j++)
-                        {
-                            if (i == 0 && j == 0)
-                            {
-                                continue;
-                            }
-
-                            var neighborRow = row + i;
-                            var neighborCol = col + j;
-
-                            if (neighborRow >= 0 && neighborRow < board.Rows && neighborCol >= 0 && neighborCol < board.Columns)
-                            {
-                                neighbors.Add(board.Cells[neighborRow, neighborCol]);
-                            }
-                        }
-                    }
-                    cell.Neighbors = neighbors.ToArray();
-                    cellControl.Cell = cell;
-                }
-            }
-        }
-
-        private List<Cell> GetNeighbors(Cell cell)
-        {
-            List<Cell> neighbors = new List<Cell>();
-
-            int row = cell.Row;
-            int col = cell.Column;
-
-            for (int i = row - 1; i <= row + 1; i++)
-            {
-                for (int j = col - 1; j <= col + 1; j++)
-                {
-                    if (i >= 0 && i < board.Rows && j >= 0 && j < board.Columns && (i != row || j != col))
-                    {
-                        neighbors.Add(board.Cells[i, j]);
-                    }
-                }
-            }
-
-            return neighbors;
-        }
-
-        private void CellControl_Revealed(object sender, EventArgs e)
-        {
-            var cellControl = (CellControl)sender;
-            var cell = cellControl.Cell;
-
-            if (cell.IsMine)
-            {
-                // Game Over!
-                cellControl.BackColor = System.Drawing.Color.Red;
-                cellControl.Image = Properties.Resources.Mine;
-                GameOver?.Invoke(this, EventArgs.Empty);
-            }
-            else if (cell.AdjacentMines == 0)
-            {
-                // Recursively reveal neighboring cells
-                cellControl.BackColor = System.Drawing.Color.LightGray;
-                cellControl.RevealNeighbors();
-            }
-            else
-            {
-                cellControl.BackColor = System.Drawing.Color.LightGray;
-                cellControl.Reveal();
-            }
-        }
-
-        private void CellControl_Flagged(object sender, EventArgs e)
-        {
-            var cellControl = (CellControl)sender;
-            var cell = cellControl.Cell;
-
-            cell.IsFlagged = !cell.IsFlagged;
-
-            if (cell.IsFlagged)
-            {
-                cellControl.Image = Properties.Resources.Flag;
-            }
-            else
-            {
-                cellControl.Image = Properties.Resources.CellCovered;
-            }
-        }
-
-        private void RevealCell(int row, int col)
-        {
-            var cell = board.Cells[row, col];
-
-            if (cell.IsRevealed)
-            {
-                return;
-            }
-
-            cell.IsRevealed = true;
-            cell.Invalidate();
-
-            if (cell.AdjacentMines == 0)
-            {
-                // Reveal all adjacent cells with no adjacent mines
-                for (int i = Math.Max(row - 1, 0); i <= Math.Min(row + 1, board.Rows - 1); i++)
-                {
-                    for (int j = Math.Max(col - 1, 0); j <= Math.Min(col + 1, board.Columns - 1); j++)
-                    {
-                        if (i != row || j != col)
-                        {
-                            RevealCell(i, j);
-                        }
-                    }
-                }
-            }
-        }
-
-
-        private void Cell_MouseDown(object sender, MouseEventArgs e)
-        {
-            var cell = (CellControl)sender;
-
-            if (e.Button == MouseButtons.Left)
-            {
-                // Left mouse button was clicked
-                if (!cell.IsFlagged && !cell.IsRevealed)
-                {
-                    if (cell.IsMine)
-                    {
-                        // Game over - reveal all mines
-                        RevealMines();
-                        MessageBox.Show("Game over!");
-                    }
-                    else
-                    {
-                        // Reveal the cell and any adjacent cells with no adjacent mines
-                        RevealCell(cell.Row, cell.Col);
-                        CheckForWin();
-                    }
-                }
-            }
-            else if (e.Button == MouseButtons.Right)
-            {
-                // Right mouse button was clicked
-                if (!cell.IsRevealed)
-                {
-                    cell.IsFlagged = !cell.IsFlagged;
-                    cell.Invalidate();
-                }
-            }
-        }
-
-        private void CheckForWin()
-        {
-            bool allNonMinesRevealed = true;
-            foreach (var cell in board.Cells)
-            {
-                if (!cell.IsMine && !cellControl[cell.Row, cell.Column].IsRevealed)
-                {
-                    allNonMinesRevealed = false;
-                    break;
-                }
-            }
-            if (allNonMinesRevealed)
-            {
-                MessageBox.Show("You win!");
-            }
-        }
-
-
-        public void Reset()
-        {
-            board.Reset();
-
-            foreach (var cellControl in cellControls)
-            {
-                cellControl.Reset();
-            }
+                break;
+            case CellState.Mine:
+                cellControl.Enabled = false;
+                cellControl.BackgroundImage = Resources.mine;
+                break;
+            default:
+                throw new InvalidOperationException("Invalid cell state.");
         }
     }
 }
